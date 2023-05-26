2ed31b262127825f1ad0e27b04d8cc07318d4583
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2023-05-25 17:11:50 -0700

    Fixed typo in 7seg digit routine

--- Core/Src/7seg.c
+++ Core/Src/7seg.c
@@ -9,7 +9,7 @@
 
 
 void writeDigit(uint_fast8_t digit) {
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, digit & 0x04);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, digit & 0x08);
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, digit & 0x04);
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, digit & 0x02);
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, digit & 0x01);
